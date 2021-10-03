ccdb85a504f7ec7f4cd920d07f8e5649e3872d2b
Francisco José Garcia Garcia <francisco.garcia@kimitec.com>
2021-10-03 19:59:09 +0200

    lets try AWS pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Paco!'));
+app.get('/', (req, res) => res.send('Hello Paco! AWS is Cool eih bro'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
